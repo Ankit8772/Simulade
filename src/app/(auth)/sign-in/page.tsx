aaaf1c164a30d2ba881e5cccad43e2fcb263f008
Ankit Kumar <aankit95087@gmail.com>
2025-08-21 02:50:30 +0530

    05: authentication socials

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,19 @@
+import { Card } from '@/components/ui/card'
+import { auth } from '@/lib/auth';
+import { SignInView } from '@/modules/auth/ui/views/SignInView'
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
+import React from 'react'
+
+const page = async() => {
+    const session = await auth.api.getSession({
+      headers:await headers(),
+    });
+  
+    if(!!session){
+      redirect("/");
+    }
+  return <SignInView/>
+}
+
+export default page
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,79 +1,17 @@
-"use client";
+import { HomeView } from "@/modules/home/ui/views/home-views"
+import { redirect } from "next/navigation";
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth";
 
-import { useState } from "react";
+const Page = async() => {
+  const session = await auth.api.getSession({
+    headers:await headers(),
+  });
 
-import { authClient } from "@/lib/auth-client";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-
-
-export default function Home() {
-  const {data: session} = authClient.useSession() 
-
-  const [name,setName] = useState("");
-  const [email,setEmail] = useState("");
-  const [password,setPassword]=useState("");
-
-  const onSumbit = () =>{
-    authClient.signUp.email({
-      email,
-      name,
-      password,
-
-    },{
-      onError: () => {
-        window.alert("Something went wrong");
-      },
-      onSuccess:() =>{
-        window.alert("Success")
-      }
-    });
+  if(!session){
+    redirect("/sign-in");
   }
+  return <HomeView />
+}
 
-    const onLogin = () =>{
-    authClient.signIn.email({
-      email,
-      password,
-
-    },{
-      onError: () => {
-        window.alert("Something went wrong");
-      },
-      onSuccess:() =>{
-        window.alert("Success")
-      }
-    });
-  }
-
-  if(session){
-    return (
-      <div className="flex flex-col p-4 gap-y-4">
-        <p>Logged in as {session.user.name} </p>
-        <Button onClick={() => authClient.signOut()}>
-          Sign out
-        </Button>
-      </div>
-    );
-  }
-
-  return (
-    <div className="flex flex-col gap-y-10">
-      <div className="p-4 flex flex-col gap-y-4">
-      <Input placeholder="name" value={name} onChange={(e) => setName(e.target.value)} />
-      <Input placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-      <Input placeholder="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <Button onClick={onSumbit}>
-        Create User
-      </Button>
-    </div>
-    <div className="p-4 flex flex-col gap-y-4">
-      <Input placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-      <Input placeholder="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <Button onClick={onLogin}>
-        Login
-      </Button>
-    </div>
-    </div>
-
-  );
-};
+export default Page;

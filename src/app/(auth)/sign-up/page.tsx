957c8ed8885c0bae15f151e46e1509e918975ae3
Ankit Kumar <aankit95087@gmail.com>
2025-08-20 23:42:42 +0530

    04: authentication-ui

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import { Card } from '@/components/ui/card'
+import { SignUpView } from '@/modules/auth/ui/views/SignUpViews'
+import React from 'react'
+
+const page = () => {
+  return <SignUpView/>
+}
+
+export default page;

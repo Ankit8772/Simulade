c2867d23302cdf8345cc62e31287fa3e597e44b4
Ankit Kumar <aankit95087@gmail.com>
2025-08-21 03:00:04 +0530

    05: authentication socials

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,8 @@
-import { Card } from '@/components/ui/card'
 import { auth } from '@/lib/auth';
 import { SignInView } from '@/modules/auth/ui/views/SignInView'
 import { headers } from 'next/headers';
 import { redirect } from 'next/navigation';
-import React from 'react'
+
 
 const page = async() => {
     const session = await auth.api.getSession({

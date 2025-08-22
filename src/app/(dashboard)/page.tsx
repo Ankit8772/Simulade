1ab012fb6fe184aef3d505bc8a849face395542b
Ankit Kumar <aankit95087@gmail.com>
2025-08-23 01:53:51 +0530

    pushed from terminal

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -3,7 +3,9 @@ import { redirect } from "next/navigation";
 import { headers } from "next/headers";
 import { auth } from "@/lib/auth";
 
+
 const Page = async() => {
+  
   const session = await auth.api.getSession({
     headers:await headers(),
   });
@@ -11,7 +13,8 @@ const Page = async() => {
   if(!session){
     redirect("/sign-in");
   }
+  
   return <HomeView />
 }
 
-export default Page;
+export default Page; 
